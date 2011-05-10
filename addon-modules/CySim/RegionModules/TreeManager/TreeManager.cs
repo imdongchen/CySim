1feb9f9c8448ad6dc3ef82c76d73ec63bcd80fc5
dongchen.cd89@gmail.com <pku.xiao@gmail.com>
2011-05-10 16:10:55 +0800

    Plant trees with a shapefile, position offset is a random from 0 to 1(hard coded) meter, and height offset 20%(hard coded) of the original.

--- addon-modules/CySim/RegionModules/TreeManager/TreeManager.cs
+++ addon-modules/CySim/RegionModules/TreeManager/TreeManager.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿    using System;
 using System.Collections.Generic;
 using System.Reflection;
 using log4net;
@@ -11,6 +11,7 @@ using OpenSim.Region.Framework.Interfaces;
 using OpenSim.Region.Framework.Scenes;
 using OpenSim.Services.Interfaces;
 using OpenSim.Region.CoreModules.Framework.InterfaceCommander;
+using SharpMap.Geometries;
 
 [assembly: Addin("CySimRegionModuleTree", "0.1")]
 [assembly: AddinDependency("OpenSim", "0.5")]
@@ -83,16 +84,10 @@ namespace CySim.RegionModules.TreeManager
 
         public void HandleTreeManageCommand(string module, string[] cmd)
         {
-            if ((Scene)MainConsole.Instance.ConsoleScene != null)
-            {
-                foreach (Scene s in m_SceneList)
-                    ProcessCommand(s, cmd);
-            }
-            else
-                ProcessCommand((Scene)MainConsole.Instance.ConsoleScene, cmd);
+             ProcessCommand(cmd);
         }
 
-        bool ProcessCommand(Scene scene, string[] cmd)
+        bool ProcessCommand(string[] cmd)
         {
             if (cmd.Length < 2)
             {
@@ -103,23 +98,43 @@ namespace CySim.RegionModules.TreeManager
             switch (cmd[1])
             {
                 case "clean":
-                    RemoveTrees(scene);
+                    if (MainConsole.Instance.ConsoleScene == null)
+                    {
+                        foreach (Scene scene in m_SceneList)
+                            RemoveTrees(scene);
+                    }
+                    else
+                        RemoveTrees((Scene)MainConsole.Instance.ConsoleScene);
+                    break;
+                case "plant":
+                    if (cmd.Length < 3)
+                    {
+                        MainConsole.Instance.Output("Syntax: cysim plant <shapefile>");
+                        return false;
+                    }
+                    
+                    try
+                    {
+                        PlantTrees(cmd[2]);
+                    }
+                    catch (Exception e)
+                    {
+                        m_log.ErrorFormat("Plant trees failed with {0} {1}", e.Message, e.StackTrace);
+                    }
                     break;
                 default:
                     return false;
             }
-
             return true;
         }
 
-        void RemoveTrees(Scene scene)
+        private void RemoveTrees(Scene scene)
         {
             foreach (ISceneEntity entity in scene.Entities) 
             {
                 if (entity is SceneObjectGroup)
                 {
                     PrimitiveBaseShape  shape = ((SceneObjectGroup)entity).RootPart.Shape;
-                    
                     if (shape.PCode == (byte)PCode.Tree || shape.PCode == (byte)PCode.NewTree)
                     {
                         scene.DeleteSceneObject((SceneObjectGroup)entity, false);
@@ -127,5 +142,89 @@ namespace CySim.RegionModules.TreeManager
                 }
             }
         }
+
+        private void PlantTrees(string shapefile)
+        {
+            List<Geometry> features;
+            List<int> types;
+            List<double> heights;
+            List<double> intervals;
+            List<Point> rasterizedPoints = new List<Point>();
+            List<Vector2> treePoints = new List<Vector2>();
+            
+            int flag = Utility.ReadShapefile(shapefile, out features, out intervals, out types, out heights);
+            switch (flag)
+            {
+                case 0:
+                    for (int i = 0, len = features.Count; i < len; i++)
+                    {
+                        Point p = features[i] as Point;
+                        Vector2 newPoint = new Vector2((float)p.X, (float)p.Y);
+                        heights[i] += Util.RandomClass.Next((int)(heights[i] * 0.2));
+                        AddTree(heights[i], newPoint, types[i]);
+                    }
+                    break;
+                case 1:
+                    for (int i = 0, len = features.Count; i < len; i++)
+                    {
+                        LineString line = features[i] as LineString;
+                        List<Point> pointList = Utility.RasterizeLine(line, intervals[i]);
+                        foreach (Point p in pointList)
+                        {
+                            Vector2 newPoint = new Vector2();
+                            newPoint.X = (float)(p.X + Util.RandomClass.Next(1));
+                            newPoint.Y = (float)(p.Y + Util.RandomClass.Next(1));
+                            heights[i] += Util.RandomClass.Next((int)(heights[i] * 0.2));
+                            AddTree(heights[i], newPoint, types[i]);
+                        }
+                    }
+                    break;
+                case 2:
+                    for (int i = 0, len = features.Count; i < len; i++)
+                    {
+                        Polygon polygon = features[i] as Polygon;
+                        List<Point> pointList = Utility.RasterizeArea(polygon, intervals[i]);
+                        foreach (Point p in pointList)
+                        {
+                            Vector2 newPoint = new Vector2();
+                            newPoint.X = (float)(p.X + Util.RandomClass.Next(1));
+                            newPoint.Y = (float)(p.Y + Util.RandomClass.Next(1));
+                            heights[i] += Util.RandomClass.Next((int)(heights[i] * 0.2));
+                            AddTree(heights[i], newPoint, types[i]);
+                        }
+                    }
+                    break;
+                default:
+                    throw new Exception("Shape type not supported!");
+            }
+        }
+
+        private void AddTree(double height, Vector2 pos, int treeType)
+        {
+            int x = (int)pos.X / 256;
+            int y = (int)pos.Y / 256;
+            foreach (Scene scene in m_SceneList)
+            {
+                uint locX = scene.RegionInfo.RegionLocX;
+                uint locY = scene.RegionInfo.RegionLocY;
+                if (x == locX && y == locY)
+                {
+                    float groupX = pos.X - 256 * x;
+                    float groupY = pos.Y - 256 * y;
+                    float groupZ = (float)scene.Heightmap[(int)groupX, (int)groupY];
+                    Vector3 position = new Vector3(groupX, groupY, groupZ);
+                    Vector3 scale = new Vector3((float)height, (float)height, (float)height);
+                    Quaternion rotation = new Quaternion(0, 0, (float)Util.RandomClass.NextDouble(), (float)Util.RandomClass.NextDouble());
+                    PrimitiveBaseShape treeShape = new PrimitiveBaseShape();
+                    treeShape.PathCurve = 16;
+                    treeShape.PathEnd = 49900;
+                    treeShape.PCode = (byte)PCode.Tree; //newTree flag needed or not?
+                    treeShape.Scale = scale;
+                    treeShape.State = (byte)treeType;
+                    scene.AddNewPrim(UUID.Random(), UUID.Random(), position, rotation, treeShape);
+                }
+            }
+        }
+
     }
 }
---
+++ addon-modules/CySim/RegionModules/TreeManager/Utility.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenMetaverse;
+using SharpMap.Geometries;
+using SharpMap.Data.Providers;
+using SharpMap.Data;
+
+namespace CySim.RegionModules.TreeManager
+{
+    public class Utility
+    {
+        public static int ReadShapefile(string file, out List<Geometry> features, out List<double> intervals, out List<int> types, out List<double> heights)
+        {
+            int flag = 0;
+            features = new List<Geometry>();
+            types = new List<int>();
+            heights = new List<double>();
+            intervals = new List<double>();
+            ShapeFile shp = null;
+
+            try
+            {
+                shp = new ShapeFile(file);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("ShapeFile not exists! " + e.Message + e.StackTrace);
+            }
+            shp.Open();
+            switch (shp.ShapeType)
+            {
+                case ShapeType.Point:
+                    flag = 0;
+                    break;
+                case ShapeType.PolyLine:
+                    flag = 1;
+                    break;
+                case ShapeType.Polygon:
+                    flag = 2;
+                    break;
+                default:
+                    flag = -1;
+                    return flag;
+            }
+
+            FeatureDataSet ds = new FeatureDataSet();
+            BoundingBox bbox = shp.GetExtents();
+            shp.ExecuteIntersectionQuery(bbox, ds);
+            FeatureDataTable table = ds.Tables[0] as FeatureDataTable;
+            foreach (FeatureDataRow row in table.Rows)
+            {
+                features.Add(row.Geometry);
+                heights.Add(Convert.ToDouble(row["Height"]));
+                types.Add(Convert.ToInt32(row["Type"]));
+                if (flag != 0)
+                    intervals.Add(Convert.ToDouble(row["Interval"]));
+            }
+            
+            shp.Close();
+            return flag;
+        }
+
+        public static double NormalDistribute(double mu, double sigma)
+        {
+            Random rnd = new Random();
+            double a = rnd.NextDouble();
+            double b = rnd.NextDouble();
+            return Math.Sqrt(-2 * Math.Log10(a)) * Math.Cos(2 * Math.PI * b) * sigma + mu;
+        }
+
+        
+        public static List<Point> RasterizeLine(LineString line, double interval)
+        {
+            List<Point> result = new List<Point>();
+            //distances[i] stores the distance from nodes[0] to nodes[i] along the line
+            List<double> distances = new List<double>();
+            IList<Point> nodes = line.Vertices;
+            
+            distances.Add(0);
+            for (int i = 1, len = nodes.Count; i < len; i++)
+            {
+                double distance = GetDistance(nodes[i - 1], nodes[i]);
+                distances.Add(distance + distances[i-1]);
+            }
+
+            result.Add(nodes[0]);
+            int index = 0;
+            for (double sum = interval, length = distances[distances.Count - 1]; sum <= length; sum += interval)
+            {
+                for (int i = index, len = distances.Count; i < len; i++)
+                {
+                    if (sum <= distances[i])
+                    {
+                        index = i;
+                        break;
+                    }
+                }
+                Point node = Interpolate(nodes[index-1], nodes[index], (sum - distances[index-1]) / (distances[index] - distances[index-1]));
+                result.Add(node);
+            }
+            return result;
+        }
+
+        public static List<Point> RasterizeArea(Polygon polygon, double interval)
+        {
+            List<Point> result = new List<Point>();
+            BoundingBox bbox = polygon.GetBoundingBox();
+            LinearRing exRing = polygon.ExteriorRing;
+            IList<LinearRing> inRings = polygon.InteriorRings;
+            int inRingsNum = polygon.NumInteriorRing;
+
+            for (double y = bbox.Bottom; y <= bbox.Top; y += interval)
+                for (double x = bbox.Left; x <= bbox.Right; x += interval)
+                {
+                    Point p = new Point(x, y);
+                    if (exRing.IsPointWithin(p))
+                    {
+                        if (inRingsNum == 0)
+                            result.Add(p);
+                        else
+                        {
+                            int index = 0;
+                            for ( ; index < inRingsNum; index++)
+                                if (inRings[index].IsPointWithin(p))
+                                    break;
+                            if (index == inRingsNum)
+                                result.Add(p);
+                        }
+                    }
+                }
+            return result;   
+        }
+
+        public static double GetDistance(Point node1, Point node2)
+        {
+            double difx = node1.X - node2.X;
+            double dify = node1.Y - node2.Y;
+            return Math.Sqrt(difx * difx + dify * dify);
+        }
+
+        public static Point Interpolate(Point node1, Point node2, double ratio)
+        {
+            double x = node1.X + (node2.X - node1.X) * ratio;
+            double y = node1.Y + (node2.Y - node1.Y) * ratio;
+            return new Point(x, y);
+        }
+    }
+}
